99d341114a87b6bf8dd7126238e9d429616f098a
forwardomg <forwardomg@gmail.com>
2016-04-03 01:50:14 +0300

    browsersync added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
 
 gulp.task('style', function () {
   return gulp.src('./scss/style.scss')
@@ -8,5 +9,22 @@ gulp.task('style', function () {
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
 });
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('watch', function() {
+  gulp.watch("scss/**/*.scss", ['style']);
+  gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
